1a3081b72047b7686fbad34b8595afaf1a7c4e66
pvshebarshin <pvshebarshin@edu.hse.ru>
2021-12-14 15:19:21 +0300

    Add Measuring and AlgorithmsResultCalculator classes and dependencies

---
+++ src/main/java/calculations/AlgorithmsResultCalculator.java
@@ -0,0 +1,32 @@
+package calculations;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.gce.geotiff.GeoTiffReader;
+
+import java.awt.image.DataBuffer;
+import java.awt.image.Raster;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+
+public final class AlgorithmsResultCalculator {
+
+    private static double[] parseRasterFile(String fileName) throws IOException {
+        GeoTiffReader reader = new GeoTiffReader(new File(fileName));
+        GridCoverage2D coverage = reader.read(null);
+
+        if (coverage == null) {
+            throw new IllegalArgumentException("Incorrect file name:" + fileName);
+        }
+
+        RenderedImage image = coverage.getRenderedImage();
+        Raster inputRaster = image.getData();
+
+        DataBuffer originalImage = inputRaster.getDataBuffer();
+        double[] newByteArray = new double[inputRaster.getWidth() * inputRaster.getHeight()];
+        for (int i = 0; i < originalImage.getSize(); i++) {
+            newByteArray[i] = originalImage.getElemDouble(i);
+        }
+        return newByteArray;
+    }
+}
---
+++ src/main/java/calculations/Measuring.java
@@ -0,0 +1,61 @@
+package calculations;
+
+public class Measuring {
+
+    private String name;
+    private double ratio;
+    private long time;
+    private long size;
+    private String parameters;
+    private String type;
+
+    public Measuring(String name, double ratio, long time, long size, String parameters, String type) {
+        this.name = name;
+        this.ratio = ratio;
+        this.time = time;
+        this.size = size;
+        this.parameters = parameters;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return name + "," + ratio + "," + time + "," + size + "," + parameters + "," + type;
+    }
+
+    public class Builder {
+
+        private Builder() {
+        }
+
+        public Builder setName(String name) {
+            Measuring.this.name = name;
+            return this;
+        }
+
+        public Builder setName(double ratio) {
+            Measuring.this.ratio = ratio;
+            return this;
+        }
+
+        public Builder setTime(long time) {
+            Measuring.this.time = time;
+            return this;
+        }
+
+        public Builder setSize(long size) {
+            Measuring.this.size = size;
+            return this;
+        }
+
+        public Builder setParameters(String parameters) {
+            Measuring.this.parameters = parameters;
+            return this;
+        }
+
+        public Builder setType(String type) {
+            Measuring.this.type = type;
+            return this;
+        }
+    }
+}
